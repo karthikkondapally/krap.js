0c5bd68004238724fea426cb7c70640ccd2c6f46
kondapally <kondapally9@gmail.com>
2016-10-12 10:20:48 +0530

    basic pie generation

---
+++ krap.js
@@ -0,0 +1,198 @@
+
+var krapAttr = {
+    width: 'width',
+    height: 'height',
+    id: 'id',
+    class: 'class',
+    undefined: 'undefined'
+};
+var errorMessages = {
+    inf: 'id is not found',
+    stw: 'How did it come here. Some thing went seriously wrong.',
+    cou: 'Create object first and use it'
+};
+var fnSvgElement = {
+    newElement: 'undefined',
+    createNewElement: function (element) {
+        this.newElement = document.createElementNS("http://www.w3.org/2000/svg", element);
+        return this;
+    },
+    addAttribute: function (key, value) {
+        if (this.newElement !== 'undefined')
+            this.newElement.setAttribute(key, value);
+        else {
+            throw 'createNewElement then add attribute';
+        }
+        return this;
+    },
+    addText: function (text) {
+        if (this.newElement !== 'undefined') {
+            this.newElement.appendChild(document.createTextNode(text));
+        } else {
+            throw 'createNewElement then add text';
+        }
+        return this;
+    },
+    toDomString: function () {
+        return this.newElement;
+    }
+};
+
+
+var krapUtil ={
+    calcTot : function(data){
+        var tot = data.reduce(function(previousValue, currentValue) {
+  return previousValue + currentValue;
+});
+    return tot;
+    },
+    getPosition: function (el) {
+            var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { x: rect.top + scrollTop, y: rect.left + scrollLeft };
+}
+}
+
+function generateArcSector(svg,cX,cY,radius,datum,colour,iniA,endA,sweep){
+cX = parseInt(cX);
+cY = parseInt(cY);
+radius = parseInt(radius);
+iniA = parseInt(iniA);
+var x1 = cX + radius * Math.cos((iniA) * Math.PI/180 )
+var y1 = cY + radius * Math.sin((iniA) * Math.PI/180 )
+
+var x2 = cX + radius * Math.cos((endA) * Math.PI/180 )
+var y2 = cY + radius * Math.sin((endA) * Math.PI/180 )
+
+var path = 'M'+cX+','+cY+' L'+x1+','+y1+' A'+radius+','+radius+' 0 '+sweep+',1 '+x2+','+y2+' z';
+console.log(path);
+var pathNode = fnSvgElement.createNewElement('path')
+			.addAttribute('d',path)
+                        .addAttribute('stroke-width','0.1')
+                        .addAttribute('class','conic')
+                        .addAttribute('stroke','black')
+                        .addAttribute('data-name','test')
+                        .addAttribute('fill',colour)
+                        .toDomString();
+    svg.appendChild(pathNode);
+}
+var sketch = {
+};
+
+krapException = {
+    generalCheck: function (actual, expected) {
+        if (actual === expected) {
+            return !0;
+        } else {
+            return !1;
+        }
+    },
+    checkId: function (id) {
+        if (this.generalCheck(id, null)) {
+            throw Error(errorMessages.inf);
+        } else {
+            return !0;
+        }
+    },
+    checkObject: function (obj) {
+        if (this.generalCheck(obj, krapAttr.undefined)) {
+            throw Error(errorMessages.cou);
+        } else {
+            return !0;
+        }
+    }
+
+};
+
+var krapPie = {
+    svgObject: 'undefined',
+    properties: {
+        width: '',
+        height: '',
+        data: [],
+        colours: []
+    },
+    create: function (eleId) {
+        var div = document.getElementById(eleId);
+        if (krapException.checkId(div)) {
+            this.svgObject = fnSvgElement.createNewElement('svg')
+                    .addAttribute('width',this.properties.width)
+                    .addAttribute('height',this.properties.height)
+                    .toDomString();
+            div.appendChild(this.svgObject);
+            return this;
+        } else {
+            throw Error(errorMessages.stw);
+        }
+    },
+    generate: function () {
+        var ht = parseInt(this.properties.height);
+        var wt = parseInt(this.properties.width);
+        var tot = krapUtil.calcTot(this.properties.data);
+        var radius = (ht+wt)/4-(0.3*((ht+wt)/4));
+        console.log('radius'+' '+((ht+wt)/2)+' '+(0.3*((ht+wt)/2))+' '+radius);
+        var len = this.properties.data.length;
+        var cl  = this.properties.colours.length;
+        var iniA =0;
+        var endA =0;
+        var cord = krapUtil.getPosition(this.svgObject);
+        console.log(cord);
+        var cx = parseInt(cord.x) + (wt/2);
+        var cy = parseInt(cord.y) + (ht/2);
+        var sweep =0;
+     gobj = fnSvgElement.createNewElement('g')
+             .addAttribute('transform','translate(-8,-8)')
+                        .toDomString();
+            
+        for(var i=0;i<len;i++){
+            endA = iniA+((360*this.properties.data[i])/tot);
+            console.log(cx+' '+cy+' '+this.properties.data+' '+len+' '+iniA+' '+endA+' '+tot+' '+radius);
+
+            if(this.properties.data[i]>(tot/2))
+            {
+                sweep=1;
+            }
+            generateArcSector(gobj,cx,cy,radius,this.properties.data[i],this.properties.colours[i%cl],iniA,endA,sweep);
+            iniA=endA;
+
+        }
+        this.svgObject.appendChild(gobj);
+    },
+    attribute: function (attr, value) {
+        krapException.checkObject(this.svgObject);
+        this.properties[attr] = value;
+        this.svgObject.setAttribute(attr, value);
+        return this;
+    },
+    data: function (dt) {
+        krapException.checkObject(this.svgObject);
+        this.properties.data = dt;
+        this.generate();
+        return this;
+    },
+};
+
+
+
+var axis = {
+};
+
+
+
+var chart = {
+    pie: function (id,props) {
+        for(var i in props){
+            krapPie.properties[i] = props[i];
+            
+        }
+        krapPie.create(id);
+        krapPie.generate();
+    },
+    bar: function () {
+
+    },
+    line: function () {
+
+    }
+};
