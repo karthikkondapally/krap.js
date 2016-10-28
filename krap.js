ac1617ee450c31d66abb573675a883c6521d50f1
kondapally <kondapally9@gmail.com>
2016-10-28 08:53:10 +0530

    added generalized axis

--- krap.js
+++ krap.js
@@ -39,57 +39,56 @@ var fnSvgElement = {
 };
 
 
-var krapUtil ={
-    calcTot : function(data){
-        var tot = data.reduce(function(previousValue, currentValue) {
-  return previousValue + currentValue;
-});
-    return tot;
+var krapUtil = {
+    calcTot: function (data) {
+        var tot = data.reduce(function (previousValue, currentValue) {
+            return previousValue + currentValue;
+        });
+        return tot;
     },
     getPosition: function (el) {
-            var rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { x: rect.top + scrollTop, y: rect.left + scrollLeft };
-}
-};
-
-var krapStats ={
-    calcRangeOnSorted : function (arr){
-     var len = arr.length;
-     var range = (arr[length-1]-arr[0])/(len-1);
-     return range;
-    },
-    
-    calcRangeOnNonSortedData : function (arr){
-     arr.sort();
-     var len = arr.length;
-     var range = (arr[length-1]-arr[0])/(len-1);
-     return range;
+        var rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {x: rect.top + scrollTop, y: rect.left + scrollLeft};
     }
 };
 
-function generateArcSector(svg,cX,cY,radius,datum,colour,iniA,endA,sweep){
-cX = parseInt(cX);
-cY = parseInt(cY);
-radius = parseInt(radius);
-iniA = parseInt(iniA);
-var x1 = cX + radius * Math.cos((iniA) * Math.PI/180 )
-var y1 = cY + radius * Math.sin((iniA) * Math.PI/180 )
+var krapStats = {
+    calcRangeOnSorted: function (arr) {
+        var len = arr.length;
+        var range = (arr[length - 1] - arr[0]) / (len - 1);
+        return range;
+    },
+    calcRangeOnNonSortedData: function (arr) {
+        arr.sort();
+        var len = arr.length;
+        var range = (arr[length - 1] - arr[0]) / (len - 1);
+        return range;
+    }
+};
 
-var x2 = cX + radius * Math.cos((endA) * Math.PI/180 )
-var y2 = cY + radius * Math.sin((endA) * Math.PI/180 )
+function generateArcSector(svg, cX, cY, radius, datum, colour, iniA, endA, sweep) {
+    cX = parseInt(cX);
+    cY = parseInt(cY);
+    radius = parseInt(radius);
+    iniA = parseInt(iniA);
+    var x1 = cX + radius * Math.cos((iniA) * Math.PI / 180)
+    var y1 = cY + radius * Math.sin((iniA) * Math.PI / 180)
 
-var path = 'M'+cX+','+cY+' L'+x1+','+y1+' A'+radius+','+radius+' 0 '+sweep+',1 '+x2+','+y2+' z';
-console.log(path);
-var pathNode = fnSvgElement.createNewElement('path')
-			.addAttribute('d',path)
-                        .addAttribute('stroke-width','0.1')
-                        .addAttribute('class','conic')
-                        .addAttribute('stroke','black')
-                        .addAttribute('data-name','test')
-                        .addAttribute('fill',colour)
-                        .toDomString();
+    var x2 = cX + radius * Math.cos((endA) * Math.PI / 180)
+    var y2 = cY + radius * Math.sin((endA) * Math.PI / 180)
+
+    var path = 'M' + cX + ',' + cY + ' L' + x1 + ',' + y1 + ' A' + radius + ',' + radius + ' 0 ' + sweep + ',1 ' + x2 + ',' + y2 + ' z';
+    console.log(path);
+    var pathNode = fnSvgElement.createNewElement('path')
+            .addAttribute('d', path)
+            .addAttribute('stroke-width', '0.1')
+            .addAttribute('class', 'conic')
+            .addAttribute('stroke', 'black')
+            .addAttribute('data-name', 'test')
+            .addAttribute('fill', colour)
+            .toDomString();
     svg.appendChild(pathNode);
 }
 var sketch = {
@@ -132,8 +131,8 @@ var krapPie = {
         var div = document.getElementById(eleId);
         if (krapException.checkId(div)) {
             this.svgObject = fnSvgElement.createNewElement('svg')
-                    .addAttribute('width',this.properties.width)
-                    .addAttribute('height',this.properties.height)
+                    .addAttribute('width', this.properties.width)
+                    .addAttribute('height', this.properties.height)
                     .toDomString();
             div.appendChild(this.svgObject);
             return this;
@@ -145,31 +144,31 @@ var krapPie = {
         var ht = parseInt(this.properties.height);
         var wt = parseInt(this.properties.width);
         var tot = krapUtil.calcTot(this.properties.data);
-        var radius = (ht+wt)/4-(0.3*((ht+wt)/4));
-        console.log('radius'+' '+((ht+wt)/2)+' '+(0.3*((ht+wt)/2))+' '+radius);
+        var radius = (ht + wt) / 4 - (0.3 * ((ht + wt) / 4));
+        console.log('radius' + ' ' + ((ht + wt) / 2) + ' ' + (0.3 * ((ht + wt) / 2)) + ' ' + radius);
         var len = this.properties.data.length;
-        var cl  = this.properties.colours.length;
-        var iniA =0;
-        var endA =0;
+        var cl = this.properties.colours.length;
+        var iniA = 0;
+        var endA = 0;
         var cord = krapUtil.getPosition(this.svgObject);
         console.log(cord);
-        var cx = parseInt(cord.x) + (wt/2);
-        var cy = parseInt(cord.y) + (ht/2);
-        var sweep =0;
-     gobj = fnSvgElement.createNewElement('g')
-             .addAttribute('transform','translate(-8,-8)')
-                        .toDomString();
-            
-        for(var i=0;i<len;i++){
-            endA = iniA+((360*this.properties.data[i])/tot);
-            console.log(cx+' '+cy+' '+this.properties.data+' '+len+' '+iniA+' '+endA+' '+tot+' '+radius);
+        var cx = parseInt(cord.x) + (wt / 2);
+        var cy = parseInt(cord.y) + (ht / 2);
+        var sweep = 0;
+        gobj = fnSvgElement.createNewElement('g')
+                .addAttribute('transform', 'translate(-8,-8)')
+                .toDomString();
 
-            if(this.properties.data[i]>(tot/2))
+        for (var i = 0; i < len; i++) {
+            endA = iniA + ((360 * this.properties.data[i]) / tot);
+            console.log(cx + ' ' + cy + ' ' + this.properties.data + ' ' + len + ' ' + iniA + ' ' + endA + ' ' + tot + ' ' + radius);
+
+            if (this.properties.data[i] > (tot / 2))
             {
-                sweep=1;
+                sweep = 1;
             }
-            generateArcSector(gobj,cx,cy,radius,this.properties.data[i],this.properties.colours[i%cl],iniA,endA,sweep);
-            iniA=endA;
+            generateArcSector(gobj, cx, cy, radius, this.properties.data[i], this.properties.colours[i % cl], iniA, endA, sweep);
+            iniA = endA;
 
         }
         this.svgObject.appendChild(gobj);
@@ -189,74 +188,143 @@ var krapPie = {
 };
 
 var svg = {
-    svgObj : 'undefined',
-    generate : function(height,width){
-        this.svgObj =  fnSvgElement.createNewElement('svg')
-                    .addAttribute('width',width)
-                    .addAttribute('height',height)
-                    .toDomString();
-            return this.svgObj;
+    svgObj: 'undefined',
+    generate: function (height, width) {
+        this.svgObj = fnSvgElement.createNewElement('svg')
+                .addAttribute('width', width)
+                .addAttribute('height', height)
+                .toDomString();
+        return this.svgObj;
     }
 };
 
 
 var axis = {
-    
-    
-    generateSimpleAxis : function(svgId,xcords,ycords,height,width){
-        var startX = (0.03)*width ;
-        var startY = (0.05)*height;
-        var oX = startX;
-        var oY = height-(0.1 * height);
-        var endX = width - (0.06 * width) ;
-        var endY = oY;
-        var lengthOfYAxis = oY-startY;
-        var lengthOfXAxis = endX-oX;
-        
-        this.generateX();
-        
-        var axisPath = pathGenerator.move(startX,startY)+' '+pathGenerator.lineTo(oX,oY)+' '+pathGenerator.move(oX,oY)+' '+pathGenerator.lineTo(endX,endY)+' '+pathGenerator.completePath();
-        var pathNode = fnSvgElement.createNewElement('path')
-			.addAttribute('d',axisPath)
-                        .addAttribute('stroke-width','1')
-                        .addAttribute('class','axis')
-                        .addAttribute('stroke','black')
-                        .addAttribute('data-name','test')
-                        .addAttribute('fill','none')
-                        .toDomString();
-                console.log(pathNode);
-        document.getElementById(svgId).appendChild(pathNode);
+    props: {
+        SX: 0,
+        SY: 0,
+        OX: 0,
+        OY: 0,
+        EX: 0,
+        EY: 0,
+        LOX: 0,
+        LOY: 0,
+        svg: ''
     },
-    generateMesh : function(){
-        
-    } 
+    generateXAxis: function () {
+        var xPath = pathG.move(this.props.SX, this.props.SY) + ' ' + pathG.lineTo(this.props.OX, this.props.OY);
+        pathObj = fnSvgElement.createNewElement('path')
+                .addAttribute('d', xPath)
+                .addAttribute('stroke-width', '1')
+                .addAttribute('class', 'xAxis')
+                .addAttribute('stroke', 'black')
+                .addAttribute('fill', 'none')
+                .toDomString();
+        this.props.svg.appendChild(pathObj);
+
+    },
+    generateYAxis: function () {
+        var yPath = pathG.move(this.props.OX, this.props.OY) + ' ' + pathG.lineTo(this.props.EX, this.props.EY);
+        pathObj = fnSvgElement.createNewElement('path')
+                .addAttribute('d', yPath)
+                .addAttribute('stroke-width', '1')
+                .addAttribute('class', 'yAxis')
+                .addAttribute('stroke', 'black')
+                .addAttribute('fill', 'none')
+                .toDomString();
+        this.props.svg.appendChild(pathObj);
+
+
+    },
+    calculateAxis: function (width, height) {
+        var startX = this.props.SX = (0.03) * width;
+        var startY = this.props.SY = (0.05) * height;
+        var oX = this.props.OX = startX;
+        var oY = this.props.OY = height - (0.1 * height);
+        var endX = this.props.EX = width - (0.06 * width);
+        var endY = this.props.EY = oY;
+        var lengthOfYAxis = this.props.LOY = oY - startY;
+        var lengthOfXAxis = this.props.LOX = endX - oX;
+    },
+    generateSimpleAxis: function (svgObj, xcords, ycords, height, width) {
+        this.props.svg = svgObj;
+        this.calculateAxis(width, height);
+        this.generateXAxis();
+        this.generateYAxis();
+
+    },
+    generateMesh: function () {
+
+    }
 };
 
-var pathGenerator = {
-  move : function(x,y){
-    return 'M'+x+','+y;  
-  },
-  lineTo : function (x,y){
-    return 'L'+x+','+y;  
-  },
-  completePath : function(){
-    return 'z';  
-  }
+var pathG = {
+    move: function (x, y) {
+        return 'M' + x + ',' + y;
+    },
+    lineTo: function (x, y) {
+        return 'L' + x + ',' + y;
+    },
+    completePath: function () {
+        return 'z';
+    }
 };
 
 var chart = {
-    pie: function (id,props) {
-        for(var i in props){
+    pie: function (id, props) {
+        for (var i in props) {
             krapPie.properties[i] = props[i];
-            
+
         }
         krapPie.create(id);
         krapPie.generate();
     },
-    bar: function () {
+    bar: function (id, props) {
+        for (var i in props) {
+            krapBar.props[i] = props[i];
+        }
+        krapBar.generate(id);
+
 
     },
     line: function () {
 
     }
 };
+
+var krapBar = {
+    props: {
+       'width': 400,
+        'height': 400,
+        'data': {},
+        'xCords': [],
+        'yCords': [],
+        'yCordsSorted': [],
+        'divId': 'undefined',
+        'datumsPerScreen': 4,
+        'colours': {},
+        'barColours': 'blue',
+        'yTickLabels': {},
+        'svgObj': 'undefined',
+        'axisType': 'generateSimpleAxis'
+    },
+    generate: function (id, properties) {
+        this.props.divId = id;
+        for (var i in properties) {
+            this.props[i] = properties[i];
+        }
+        var data = this.props['data'];
+        var xCords = [];
+        var yCords = [];
+        for (var i in data) {
+            xCords.push(i);
+            yCords.push(data[i]);
+        }
+        this.props.xCords = xCords;
+        this.props.yCords = yCords;
+        this.props.yCordsSorted = yCords.sort();
+        this.props.svgObj = svg.generate(this.props.height, this.props.width);
+        window['axis'][this.props.axisType](this.props.svgObj, xCords, yCords, this.props.height, this.props.width);
+        document.getElementById(id).appendChild(this.props.svgObj);
+    }
+}; 
